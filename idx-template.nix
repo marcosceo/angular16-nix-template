a94d12a7341cb05b0205d311497941ce387dad05
Marcos Ceo <marcos.ceo@outlook.com>
2025-06-28 11:14:26 -0300

    Update idx-template.nix

--- idx-template.nix
+++ idx-template.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }: {
-  packages = [ pkgs.nodejs_20 ];
+  packages = [ pkgs.nodejs_22 ];
   bootstrap = ''
-    npx --prefer-offline -y @angular/cli@16.2.16 new --skip-git --defaults --skip-install --directory "$WS_NAME" "$WS_NAME"
+    npx --prefer-offline -y @angular/cli@19.2.15 new --skip-git --defaults --skip-install --directory "$WS_NAME" "$WS_NAME"
     mkdir "$WS_NAME"/.idx
     cp ${./dev.nix} "$WS_NAME"/.idx/dev.nix && chmod +w "$WS_NAME"/.idx/dev.nix
     mv "$WS_NAME" "$out"
